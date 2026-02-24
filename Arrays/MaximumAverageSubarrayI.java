9ff66f3ef11e388014f77bc37b4299dbc7daa0b2
Rameshmahara10 <mahararamesh124@gmail.com>
2026-02-24 21:04:00 +0530

    Create MaximumAverageSubarrayI.java

---
+++ Arrays/MaximumAverageSubarrayI.java
@@ -0,0 +1,25 @@
+// Problem: Maximum Average Subarray I
+// LeetCode: 643
+// Difficulty: Easy
+// Approach: Sliding Window
+// Time Complexity: O(n)
+// Space Complexity: O(1)
+
+class Solution {
+    public double findMaxAverage(int[] nums, int k) {
+
+        int sum = 0;
+        for (int i = 0; i < k; i++) {
+            sum += nums[i];
+        }
+
+        int maxSum = sum;
+
+        for (int i = k; i < nums.length; i++) {
+            sum += nums[i] - nums[i - k];
+            maxSum = Math.max(maxSum, sum);
+        }
+
+        return (double) maxSum / k;
+    }
+}
